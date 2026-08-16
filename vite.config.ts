bdd17625f0654d3941c2f47dc5b5d612fdde08cd
FlyingC41pmunk <steriophonixx@yahoo.com>
2026-08-16 17:30:29 +0100

    fixed vite config and built lore

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
-  server: { port: 1420, strictPort: true },
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/target/**"]
+    }
+  },
   envPrefix: ["VITE_", "TAURI_ENV_"],
-  build: { target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "es2022" },
+  build: {
+    target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "es2022"
+  },
   test: {
     environment: "jsdom",
     setupFiles: ["./tests/setup.ts"],
